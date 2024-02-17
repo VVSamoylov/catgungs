5dd5175fe021de6abfa7fb5f152fef752521f2b5
Veniamin <bm0651@yandex.ru>
2024-02-17 17:59:59 +0300

    lesson 1

--- README.md
+++ README.md
@@ -14,6 +14,16 @@ npm run serve
 ```
 npm run build
 ```
+https://cli.vuejs.org/ru/guide/
+https://rutube.ru/channel/20954924/videos/
+https://www.youtube.com/watch?v=CxgOKJh4zWE
+vue
+https://cli.vuejs.org/ru/guide/
+https://www.youtube.com/watch?v=XzLuMtDelGk
+https://v3.ru.vuejs.org/
+https://v3.vuex.vuejs.org/ru/
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+графика и игры
+https://www.youtube.com/@DenisKozhar/videos
+https://www.youtube.com/@itproger/videos  +vue
+
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
+<div class="container">
   <nav>
-    <router-link to="/">Home</router-link> |
+  <Menu/>
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
+  </div>
 </template>
+<script>
+// @ is an alias to /src
+import Menu from '@/components/Menu.vue'
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+export default {
+  name: 'App',
+  components: {
+    Menu
+  }
 }
+</script>
+<style scoped>
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
