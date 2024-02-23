e11a93b8c551fac2216a6b5bd152b2a624b96cf4
Veniamin <bm0651@yandex.ru>
2024-02-24 02:07:37 +0300

    add style from menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -9,10 +9,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <router-link to="/">Гавная</router-link> 
+          <router-link class="nav-link" to="/">Гавная</router-link> 
         </li>
         <li class="nav-item">
-          <router-link to="/addjob">Принять на работу</router-link> 
+          <router-link class="nav-link" to="/addjob">Принять на работу</router-link> 
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Список сотрудников</a>
